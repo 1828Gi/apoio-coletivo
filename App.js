ea24ebf3443f0d2ebf2e15edd23b35b7a4ac5784
Osvaldo Aurélio <osvaldo.ribeiro@lambda3.com.br>
2022-10-12 17:03:42 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Pressable, StyleSheet, Text, View } from 'react-native';
+import { bancoDeDados } from './api';
 
 export default function App() {
+  const { restaurantes: [restaurante1, restaurante2] } = bancoDeDados;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Pressable onPress={() => Alert.alert('furutamente vai navegar para Restaurantes')} style={styles.button}>
+        <Text style={styles.text}>Restaurantes</Text>
+      </Pressable>
+      <Pressable onPress={() => Alert.alert('furutamente vai navegar para Albergues')} style={styles.button}>
+        <Text style={styles.text}>Albergues</Text>
+      </Pressable>
+      <Pressable onPress={() => Alert.alert('furutamente vai navegar para Outros serviços')} style={styles.button}>
+        <Text style={styles.text}>Outros serviços</Text>
+      </Pressable>
+      <Text>{restaurante1.horarioFuncionamento}</Text>
+      <Text>{restaurante2.horarioFuncionamento}</Text>
     </View>
   );
 }
@@ -14,7 +27,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: '#0397a5',
+    paddingVertical: 16,
+    paddingHorizontal: 32,
+    borderRadius: 16,
+    margin: 16,
+    
+  },
+  text: {
+    color: '#fff',
+    fontSize: 24,
+  }
 });
---
+++ api/index.js
@@ -0,0 +1,26 @@
+export const bancoDeDados = {
+  albergues: [
+    {
+      endereco: 'rua santa clara',
+      quantidadeMax: 20,
+    },
+    {
+      endereco: 'rua santa escura',
+      quantidadeMax: 30,
+    },
+  ],
+  restaurantes: [
+    {
+      endereco: 'rua santa media',
+      horarioFuncionamento: '8h - 18h'
+    },
+    {
+      endereco: 'rua demonia clara',
+      horarioFuncionamento: '8h - 20h'
+    },
+    {
+      endereco: 'rua demonia escura',
+      horarioFuncionamento: '10h - 23h'
+    },
+  ]
+}
