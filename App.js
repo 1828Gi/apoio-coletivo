0b82ab56800791beb80cecbdcee478f26382920b
Giovana Marachini <giovanapalma.jus@gmail.com>
2022-10-20 22:11:06 -0300

    install navigation

--- App.js
+++ App.js
@@ -1,44 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { Alert, Pressable, StyleSheet, Text, View } from 'react-native';
-import { bancoDeDados } from './api';
+import React from "react"
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import BemVindo from "./src/telas/BemVindo";
+import Albergues from "./src/telas/Albergues";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const { restaurantes: [restaurante1, restaurante2] } = bancoDeDados;
-
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Pressable onPress={() => Alert.alert('furutamente vai navegar para Restaurantes')} style={styles.button}>
-        <Text style={styles.text}>Restaurantes</Text>
-      </Pressable>
-      <Pressable onPress={() => Alert.alert('furutamente vai navegar para Albergues')} style={styles.button}>
-        <Text style={styles.text}>Albergues</Text>
-      </Pressable>
-      <Pressable onPress={() => Alert.alert('furutamente vai navegar para Outros serviços')} style={styles.button}>
-        <Text style={styles.text}>Outros serviços</Text>
-      </Pressable>
-      <Text>{restaurante1.horarioFuncionamento}</Text>
-      <Text>{restaurante2.horarioFuncionamento}</Text>
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="BemVindo">
+        <Stack.Screen name="BemVindo" component={BemVindo} />
+        <Stack.Screen name="Albergues" component={Albergues} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-  },
-  button: {
-    backgroundColor: '#0397a5',
-    paddingVertical: 16,
-    paddingHorizontal: 32,
-    borderRadius: 16,
-    margin: 16,
-    
-  },
-  text: {
-    color: '#fff',
-    fontSize: 24,
-  }
-});
---
+++ src/telas/BemVindo/styles.js
@@ -0,0 +1,23 @@
+import {  StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      justifyContent: 'center',
+    },
+    button: {
+      backgroundColor: '#0397a5',
+      paddingVertical: 16,
+      paddingHorizontal: 32,
+      borderRadius: 16,
+      margin: 16,
+      
+    },
+    text: {
+      color: '#fff',
+      fontSize: 24,
+    }
+  });
+
+  
