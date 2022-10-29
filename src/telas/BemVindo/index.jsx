250648ad3d8f140fc67a3faa69e82e7185f35e4f
Giovana Marachini <giovanapalma.jus@gmail.com>
2022-10-29 00:15:04 -0300

    adjust in BemVindo screen

--- src/telas/BemVindo/index.jsx
+++ src/telas/BemVindo/index.jsx
@@ -1,35 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { Alert, Image, TouchableHighlight, ScrollView, Text } from 'react-native';
+import { Alert, Image, TouchableHighlight, ScrollView, Text, View } from 'react-native';
 import { styles } from './styles';
 
 export default function BemVindo({ navigation }) {
   return (
     <ScrollView style={styles.container}>
       <StatusBar style="auto" />
+      <Text style={styles.título}>Apoio Coletivo</Text>
       <Image style={styles.img} source={require('./img/aperto-de-mao.jpg')} />
       <TouchableHighlight
         underlayColor="#f7b142"
         onPress={() => navigation.navigate('Albergues')}
-        style={styles.button}>
-        <Text style={styles.text}>Albergues</Text>
+        style={styles.botão}>
+        <View style={styles.botãoContainer}>
+          <Image style={styles.botãoImagem} source={require('./img/albergue-icone-branco.png')} />
+          <Text style={styles.botãoTexto}>Albergues</Text>
+        </View>
       </TouchableHighlight>
       <TouchableHighlight
         underlayColor="#f7b142"
         onPress={() => navigation.navigate('Restaurantes')}
-        style={styles.button}>
-        <Text style={styles.text}>Restaurantes</Text>
+        style={styles.botão}>
+        <View style={styles.botãoContainer}>
+          <Image style={styles.botãoImagem} source={require('./img/icone-faca-garfo-branco.png')} />
+          <Text style={styles.botãoTexto}>Restaurantes</Text>
+        </View>
       </TouchableHighlight>
       <TouchableHighlight
         underlayColor="#f7b142"
         onPress={() => navigation.navigate('Saude')}
-        style={styles.button}>
-        <Text style={styles.text}>Saúde</Text>
+        style={styles.botão}>
+        <View style={styles.botãoContainer}>
+          <Image
+            style={[styles.botãoImagem, styles.botãoImagemAjuste]}
+            source={require('./img/icone-saude-branco.png')}
+          />
+          <Text style={styles.botãoTexto}>Saúde</Text>
+        </View>
       </TouchableHighlight>
       <TouchableHighlight
         underlayColor="#f7b142"
         onPress={() => Alert.alert('furutamente vai navegar para Outros serviços, amém')}
-        style={styles.button}>
-        <Text style={styles.text}>Outros serviços</Text>
+        style={styles.botão}>
+        <View style={styles.botãoContainer}>
+          <Image
+            style={[styles.botãoImagem, styles.botãoImagemAjuste]}
+            source={require('./img/outros-servicos-branco.png')}
+          />
+          <Text style={styles.botãoTexto}>Outros serviços</Text>
+        </View>
       </TouchableHighlight>
     </ScrollView>
   );
--- src/telas/BemVindo/styles.js
+++ src/telas/BemVindo/styles.js
@@ -5,22 +5,43 @@ export const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  button: {
-    backgroundColor: '#0397a5',
-    paddingVertical: 24,
-    paddingHorizontal: 32,
-    borderRadius: 16,
-    margin: 16,
+  título: {
+    alignSelf: 'center',
+    fontSize: 48,
+    color: '#0397a5',
+    fontWeight: 'bold',
+    marginTop: 48,
   },
-  text: {
+  botão: {
+    backgroundColor: '#0397a5',
+    paddingVertical: 16,
+    paddingHorizontal: 32,
+    borderRadius: 24,
+    margin: 12,
+  },
+  botãoContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  botãoImagem: {
+    width: 64,
+    height: 64,
+    marginRight: 16,
+  },
+  botãoImagemAjuste: {
+    width: 76,
+    marginLeft: -8,
+  },
+  botãoTexto: {
     color: '#fff',
     fontSize: 24,
+    fontWeight: 'bold',
   },
   img: {
     resizeMode: 'center',
     alignSelf: 'center',
-    width: 360,
-    height: 360,
+    width: 280,
+    height: 280,
     marginTop: 32,
   },
 });
